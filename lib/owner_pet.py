0d9e4fdbbb71ed35167afd758d7fe6ade91fa21a
Mateusz M Trybunia <Mateusz.m.trybunia@gmail.com>
2023-10-26 11:04:26 -0500

    Complete

--- lib/owner_pet.py
+++ lib/owner_pet.py
@@ -1,5 +1,40 @@
-class Pet:
-    pass
 
+
+class Pet:
+    PET_TYPES = ["dog", "cat", "rodent", "bird", "reptile", "exotic"]
+    all = []
+    def __init__(self, name="Python", pet_type="reptile", owner=None):
+        self.name = name
+        self.pet_type = pet_type
+        self.owner = owner
+        Pet.all.append(self)
+    @property
+    def pet_type(self):
+        return self._pet_type
+    @pet_type.setter
+    def pet_type(self,pet_type):
+        if pet_type in self.PET_TYPES:
+            self._pet_type = pet_type
+        else:
+            raise Exception
+    @property
+    def owner(self):
+        return self._owner
+    @owner.setter
+    def owner(self,owner):
+        if not (isinstance(owner,Owner) or not owner):
+            raise Exception
+        self._owner = owner
 class Owner:
-    pass
+    
+    def __init__(self, name):
+        self.name = name
+
+    def pets(self):
+        return [pet for pet in Pet.all if pet.owner == self]
+    def add_pet(self, pet):
+        if not isinstance(pet,Pet):
+            raise Exception
+        pet.owner = self
+    def get_sorted_pets(self):
+        return sorted(self.pets(),key=lambda pet:pet.name)
